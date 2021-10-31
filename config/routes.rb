2580c432ce6c0e18ba7cc5a217b1d61e33d79b1e
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-10-31 14:52:21 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'dishes/index'
+  root to:"dishes#index"
 end
